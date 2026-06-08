bc619046e29df207151a40c0f0b986cd8ffeb27f
Kremilly <contact@kremilly.com>
2026-06-08 03:38:30 -0300

    fix: update HTML language attribute in LaTeX template from en-US to en

--- src/generator/templates.rs
+++ src/generator/templates.rs
@@ -36,7 +36,7 @@ impl Templates {
     pub fn latex(content: &str) -> String {
         format!(
             r##"<!DOCTYPE html>
-            <html lang="en-US">
+            <html lang="en">
             <head>
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
