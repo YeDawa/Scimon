48e8e5d0f8f1fb00c89eeb09feec9990217aa364
Kremilly <contact@kremilly.com>
2026-06-26 18:18:06 -0300

    style: format MONLIB_PACKAGE_MANAGER_KEYS for improved readability

--- src/consts/bundler.rs
+++ src/consts/bundler.rs
@@ -3,7 +3,9 @@ pub struct Bundler;
 impl Bundler {
 
     // Monlib Package Manager
-    pub const MONLIB_PACKAGE_MANAGER_KEYS: [&str; 6] = ["name", "description", "author", "license", "privacy", "homepage"];
+    pub const MONLIB_PACKAGE_MANAGER_KEYS: [&str; 6] = [
+        "name", "description", "author", "license", "privacy", "homepage"
+    ];
 
     // The entry file template for a Scimon package.
     pub const ENTRY_PACKAGE: &'static str = "\
